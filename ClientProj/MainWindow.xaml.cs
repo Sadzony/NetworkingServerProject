79ee1eb15b0618ac43988918a951828e3a1514eb
Sadzony <szymon.sadunski@gmail.com>
2021-11-26 03:57:49 +0000

    Client side now has a GUI - functionality not implemented

---
+++ ClientProj/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace ClientProj
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void sendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (messageText.Text == "")
+            {
+                MessageBox.Show("No Message in text box!", "Warning");
+            }
+            else
+            {
+                string message = messageText.Text;
+                messageText.Text = "";
+                if (localName.Text == "")
+                {
+                    MessageBox.Show("Please enter a local name!", "Warning");
+                    messageText.Text = message;
+                }
+                else
+                {
+                    string name = localName.Text;
+                    message = name + " says: " + message + "\n";
+                    ThreadStart ButtonThread = delegate ()
+                    {
+                        Dispatcher.Invoke(DispatcherPriority.Normal, new Action<string>(SendMessage), message);
+                    };
+                    new Thread(ButtonThread).Start();
+
+                }
+            }
+        }
+        private void SendMessage(string status)
+        {
+            chatbox.Text = status;
+        }
+    }
+}
